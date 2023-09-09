2e09c9af7de54ea5aafceabde5bdc3c79b37e8c4
Marcos <marcosdevelopment.business@gmail.com>
2023-09-09 13:23:56 -0300

    add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +14,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='pt-BR'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          defaultTheme='light'
+          attribute='class'
+          storageKey='simple-blog-theme-configuration'
+          enableSystem>
+          <ThemeToggle />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Check, Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
+const THEMES = {
+  light: 'Claro',
+  dark: 'Escuro',
+  system: 'Sistema'
+}
+
+type Themes = (keyof typeof THEMES)[]
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          size='icon'>
+          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <span className='sr-only'>Alterar tema</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        {(Object.keys(THEMES) as Themes).map(key => (
+          <DropdownMenuItem
+            onClick={setTheme.bind(null, key)}
+            className='flex items-center justify-between'>
+            {THEMES[key]}
+            {theme === key && <Check className='w-4 h-4' />}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
